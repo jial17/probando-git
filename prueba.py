3dfbb008aa705815f441026813f96e76ea0e4ddf
jial1979 <jial17@gmailcom>
2022-08-21 10:10:00 -0500

    progra mejorado

--- prueba.py
+++ prueba.py
@@ -1,13 +1,11 @@
 altura = 0
-requeridos= 0
-
 
 n_bloques= int(input("ingrese el numero de bloques: "))
-print("numero de bloques", n_bloques)
+print("numero de bloques:", n_bloques)
 
-while requeridos < n_bloques:
+while altura < n_bloques:
     altura +=1
-    requeridos = altura
     n_bloques-= altura
 
-print("altura",altura)
+print("altura: ",altura)
+print("bloques restantes: ",n_bloques)
