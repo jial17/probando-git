525cd6d08acc9d0e2fc2e06982f25126f33333d7
jial1979 <jial17@gmailcom>
2022-08-21 10:03:17 -0500

    programa funcional

--- prueba.py
+++ prueba.py
@@ -1,7 +1,13 @@
 altura = 0
+requeridos= 0
 
 
-n_bloques= input("ingrese el numero de bloques: ")
-print(n_bloques)
+n_bloques= int(input("ingrese el numero de bloques: "))
+print("numero de bloques", n_bloques)
 
-requeridos= n_bloques-altura
+while requeridos < n_bloques:
+    altura +=1
+    requeridos = altura
+    n_bloques-= altura
+
+print("altura",altura)
